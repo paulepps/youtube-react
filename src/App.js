4c04da8c7d1bc91dafba07a856c506cb06acc7d1
Paul Epps <paul@eppsnet.com>
2020-03-20 22:32:47 -0700

    tutorial 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
 
 function App() {
   return (
     <div>
-        Hello World
+        <HeaderNav />
     </div>
   );
 }
