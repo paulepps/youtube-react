e677993bfc207ca0bb314fd5fb458914df346996
Paul Epps <paul@eppsnet.com>
2020-04-15 22:35:49 -0700

    Tutorial 7

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
+import "./Home.scss";
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <VideoPreview />
+        </div>
+    );
+};
