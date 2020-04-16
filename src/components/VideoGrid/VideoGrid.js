ceb4822462d4c9d6c95ee4750fb8a213d4a0fcda
Paul Epps <paul@eppsnet.com>
2020-04-16 15:44:54 -0700

    Tutorial 8

---
+++ src/components/VideoGrid/VideoGrid.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./VideoGrid.scss";
+import { VideoGridHeader } from "./VideoGridHeader/VideoGridHeader";
+import { Divider } from "semantic-ui-react";
+import { VideoPreview } from "../VideoPreview/VideoPreview";
+
+export function VideoGrid(props) {
+    const divider = props.hideDivider ? null : <Divider />;
+    return (
+        <>
+            <div className="video-section">
+                <VideoGridHeader title="Trending" />
+                <div className="video-grid">
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    <VideoPreview />
+                    {divider}
+                </div>
+            </div>
+        </>
+    );
+}
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
 import "./Home.scss";
+import { VideoGrid } from "../../components/VideoGrid/VideoGrid";
 
 export const Home = () => {
     return (
-        <div className="home">
-            <VideoPreview />
-        </div>
+        <>
+            <div className="home">
+                    <VideoGrid title="Trending" />
+                    <VideoGrid title="Autos & Vehicles" hideDivider={true} />
+            </div>
+        </>
     );
 };
