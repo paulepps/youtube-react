39a0109f309eda42520c9fad76c7a7671f858c26
Paul Epps <paul@eppsnet.com>
2020-04-16 21:33:11 -0700

    Tutorial 11

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Watch.scss";
+import { Video } from "../../components/Video/Video";
 
 export const Watch = () => {
-    return <div>Watch component goes here</div>;
+    return (
+        <div style={{ maxWidth: "80%" }}>
+            <Video id="-7fuHEEmEjs" />
+        </div>
+    );
 };
