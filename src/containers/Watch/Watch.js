937ce37540ff0b95865d7ef0e28036ccf569f22a
Paul Epps <paul@eppsnet.com>
2020-04-25 23:14:36 -0700

    Tutorial 15

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,9 +1,35 @@
 import React from "react";
 import "./Watch.scss";
 import { RelatedVideos } from "../../components/RelatedVideos/RelatedVideos";
+import { Video } from "../../components/Video/Video";
+import { VideoMetadata } from "../../components/VideoMetadata/VideoMetadata";
 
 export const Watch = () => {
     return (
-        <RelatedVideos />
+        <div className="watch-grid">
+            <Video className="video" id="-7fuHEEmEjs" />
+            <VideoMetadata className="metadata" viewcount={1000} />
+            <div
+                className="video-info-box"
+                style={{
+                    width: "100%",
+                    height: "100px",
+                    background: "#BD10E0",
+                }}
+            >
+                Video Info box
+            </div>
+            <div
+                className="comments"
+                style={{
+                    width: "100%",
+                    height: "100px",
+                    background: "#9013FE",
+                }}
+            >
+                comments
+            </div>
+            <RelatedVideos className="relatedVideos" />
+        </div>
     );
 };
