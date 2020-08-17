cf77dba07c23e8f935560d3cf649df39f0c9ba9f
paulepps <paul@eppsnet.com>
2020-08-16 17:57:40 -0700

    Part 32

--- src/components/VideoGrid/VideoGrid.js
+++ src/components/VideoGrid/VideoGrid.js
@@ -5,22 +5,26 @@ import { Divider } from "semantic-ui-react";
 import { VideoPreview } from "../VideoPreview/VideoPreview";
 
 export function VideoGrid(props) {
-    if (!props.videos || !props.videos.length) {
-        return <div />;
-    }
-    const gridItems = props.videos.map(video => {
-        return (<VideoPreview video={video}
-            key={video.id} />);
-    });
-
-    const divider = props.hideDivider ? null : <Divider />;
+  if (!props.videos || !props.videos.length) {
+    return <div />;
+  }
+  const gridItems = props.videos.map((video) => {
     return (
-        <React.Fragment>
-            <VideoGridHeader title={props.title} />
-            <div className='video-grid'>
-                {gridItems}
-            </div>
-            {divider}
-        </React.Fragment>
+      <VideoPreview
+        video={video}
+        key={video.id}
+        pathname="/watch"
+        search={`v=${video.id}`}
+      />
     );
+  });
+
+  const divider = props.hideDivider ? null : <Divider />;
+  return (
+    <React.Fragment>
+      <VideoGridHeader title={props.title} />
+      <div className="video-grid">{gridItems}</div>
+      {divider}
+    </React.Fragment>
+  );
 }
--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -3,18 +3,22 @@ import "./Watch.scss";
 import { RelatedVideos } from "../../components/RelatedVideos/RelatedVideos";
 import { Video } from "../../components/Video/Video";
 import { VideoMetadata } from "../../components/VideoMetadata/VideoMetadata";
-import { Image, Button } from "semantic-ui-react";
 import { VideoInfoBox } from "../../components/VideoInfoBox/VideoInfoBox";
 import { Comments } from "../Comments/Comments";
 
-export const Watch = () => {
-    return (
-        <div className="watch-grid">
-            <Video className="video" id="-7fuHEEmEjs" />
-            <VideoMetadata className="metadata" viewcount={1000} />
-            <VideoInfoBox className="video-info-box" />
-            <Comments className="comments" />
-            <RelatedVideos className="relatedVideos" />
-        </div>
-    );
+export const Watch = (props) => {
+  const getVideoId = () => {
+    const searchParams = new URLSearchParams(props.location.search);
+    return searchParams.get("v");
+  };
+
+  return (
+    <div className="watch-grid">
+      <Video className="video" id="-7fuHEEmEjs" />
+      <VideoMetadata className="metadata" viewcount={1000} />
+      <VideoInfoBox className="video-info-box" />
+      <Comments className="comments" />
+      <RelatedVideos className="relatedVideos" />
+    </div>
+  );
 };
